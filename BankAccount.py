66018b8997c53498f69957880755ee4eb7b7c293
Allen Stevens <allen.stevens@students.dominican.edu>
2023-11-01 16:45:31 -0700

    created bankaccount class and random account generator method

--- BankAccount.py
+++ BankAccount.py
@@ -1,4 +1,11 @@
 import random
-
 class BankAccount:
-  
+    def __init__(self, full_name):
+    #initialize a bank account with a full_name, generate a random 8 digit number and set inital balance to 0 
+        self.full_name = full_name
+        self.account_number = self.generate_account_number()
+        self.balance = 0
+def generate_account_number(self):
+    #generate a random 8-digit account number 
+    return str(random.randint(100000000, 99999999))
+
